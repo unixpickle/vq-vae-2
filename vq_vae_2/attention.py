f8527d3f775fb2b38f2072c025e46b5f074a1e1e
Alex Nichol <unixpickle@gmail.com>
2019-06-16 07:49:23 -0400

    fixes for attention

--- vq_vae_2/attention.py
+++ vq_vae_2/attention.py
@@ -22,16 +22,17 @@ class PixelAttention(nn.Module):
     """
 
     def __init__(self, num_channels, num_heads=8):
+        super().__init__()
         self.attention = MaskedAttention(num_channels, num_heads=num_heads)
 
-    def forward(self, *images, cond=None):
+    def forward(self, *images, conds=None):
         """
         Apply masked attention to a batch of images.
 
         Args:
             images: one or more [N x C x H x W] Tensors.
-            cond: ignored. Here for compatibility with the
-              PixelCNN aggregator.
+            conds: ignored. Here for compatibility with
+              the PixelCNN aggregator.
 
         Returns:
             A new list of [N x C x H x W] Tensors.
@@ -63,6 +64,8 @@ class MaskedAttention(nn.Module):
     """
 
     def __init__(self, num_channels, num_heads=8):
+        super().__init__()
+
         assert not num_channels % num_heads, 'heads must evenly divide channels'
         self.num_channels = num_channels
         self.num_heads = num_heads
@@ -81,12 +84,12 @@ class MaskedAttention(nn.Module):
             A new [N x T x C] Tensor.
         """
         projected = self.kqv_projection(sequence)
-        kqv = torch.split(projected, 3, dim=-1)
+        kqv = torch.split(projected, self.num_channels, dim=-1)
         keys, queries, values = [self._split_heads(x) for x in kqv]
         logits = torch.bmm(queries, keys.permute(0, 2, 1))
         logits /= math.sqrt(self.num_channels / self.num_heads)
         logits += self._logit_mask(sequence.shape[1])
-        weights = F.softmax(logits)
+        weights = F.softmax(logits, dim=-1)
         weighted_sum = torch.bmm(weights, values)
         combined = self._combine_heads(weighted_sum)
         return self.mix_heads(combined)
@@ -106,7 +109,7 @@ class MaskedAttention(nn.Module):
         num_steps = batch.shape[1]
         split_channels = self.num_channels // self.num_heads
         batch = batch.view(batch_size, num_steps, self.num_heads, split_channels)
-        batch = batch.permute(0, 2, 1, 3)
+        batch = batch.permute(0, 2, 1, 3).contiguous()
         batch = batch.view(batch_size * self.num_heads, num_steps, split_channels)
         return batch
 
@@ -124,13 +127,13 @@ class MaskedAttention(nn.Module):
         num_steps = batch.shape[1]
         split_channels = self.num_channels // self.num_heads
         batch = batch.view(batch_size, self.num_heads, num_steps, split_channels)
-        batch = batch.permute(0, 2, 1, 3)
+        batch = batch.permute(0, 2, 1, 3).contiguous()
         batch = batch.view(batch_size, num_steps, self.num_channels)
         return batch
 
     def _logit_mask(self, num_steps):
         row_indices = np.arange(num_steps)[:, None]
         col_indices = np.arange(num_steps)[None]
-        upper = (row_indices <= col_indices)
+        upper = (row_indices >= col_indices)
         mask = np.where(upper, 0, -np.inf).astype(np.float32)
         return torch.from_numpy(mask).to(next(self.parameters()).device)

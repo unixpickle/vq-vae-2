52838249b360d1c73e69d9467ebff6240b6b5c54
Alex Nichol <unixpickle@gmail.com>
2019-06-17 15:54:53 -0400

    train/test losses in mnist demo

--- vq_vae_2/examples/mnist/train_generator.py
+++ vq_vae_2/examples/mnist/train_generator.py
@@ -28,28 +28,31 @@ def main():
     optimizer = optim.Adam(generator.parameters(), lr=LR)
     loss_fn = nn.CrossEntropyLoss()
 
+    test_images = load_images(train=False)
     for batch_idx, images in enumerate(load_images()):
-        _, (_, _, encoded) = encoder(images)
-        logits = generator(encoded)
-        logits = logits.permute(0, 2, 3, 1).contiguous()
-        logits = logits.view(-1, logits.shape[-1])
-        loss = loss_fn(logits, encoded.view(-1))
+        losses = []
+        for img_set in [images, next(test_images)]:
+            _, (_, _, encoded) = encoder(img_set)
+            logits = generator(encoded)
+            logits = logits.permute(0, 2, 3, 1).contiguous()
+            logits = logits.view(-1, logits.shape[-1])
+            losses.append(loss_fn(logits, encoded.view(-1)))
         optimizer.zero_grad()
-        loss.backward()
+        losses[0].backward()
         optimizer.step()
-        print('loss=%f' % loss.item())
+        print('train=%f test=%f' % (losses[0].item(), losses[1].item()))
         if not batch_idx % 100:
             torch.save(generator.state_dict(), 'gen.pt')
 
 
-def load_images():
+def load_images(train=True):
     while True:
-        for data, _ in create_data_loader():
+        for data, _ in create_data_loader(train):
             yield data
 
 
-def create_data_loader():
-    mnist = torchvision.datasets.MNIST('./data', train=True, download=True,
+def create_data_loader(train):
+    mnist = torchvision.datasets.MNIST('./data', train=train, download=True,
                                        transform=torchvision.transforms.ToTensor())
     return torch.utils.data.DataLoader(mnist, batch_size=BATCH_SIZE, shuffle=True)
 

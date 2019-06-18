f7b693b04a6573b1dddd74de8157e91e9661ee76
Alex Nichol <unixpickle@gmail.com>
2019-06-18 15:16:15 -0400

    drafted script to train top prior

--- vq_vae_2/examples/hierarchical/model.py
+++ vq_vae_2/examples/hierarchical/model.py
@@ -2,6 +2,12 @@
 Models for hierarchical image generation.
 """
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from vq_vae_2.attention import PixelAttention
+from vq_vae_2.pixel_cnn import PixelCNN, PixelConvA, PixelConvB
 from vq_vae_2.vq_vae import HalfDecoder, HalfQuarterDecoder, HalfEncoder, QuarterEncoder, VQVAE
 
 
@@ -9,3 +15,66 @@ def make_vae():
     encoders = [QuarterEncoder(3, 128, 512), HalfEncoder(128, 128, 512)]
     decoders = [HalfDecoder(128, 128), HalfQuarterDecoder(128, 3)]
     return VQVAE(encoders, decoders)
+
+
+class TopPrior(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embed = nn.Embedding(512, 512)
+        self.pixel_cnn = PixelCNN(
+            PixelConvA(512, 512),
+
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelAttention(512),
+
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelAttention(512),
+
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelAttention(512),
+
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelConvB(512),
+            PixelAttention(512),
+        )
+        self.out_stack = nn.Sequential(
+            nn.Conv1d(1024, 512, 1),
+            Residual1x1(512),
+            Residual1x1(512),
+            Residual1x1(512),
+            Residual1x1(512),
+        )
+
+    def forward(self, x):
+        x = self.embed(x)
+        x = x.permute(0, 3, 1, 2).contiguous()
+        out1, out2 = self.pixel_cnn(x)
+        return self.out_stack(torch.cat([out1, out2], dim=1))
+
+
+class Residual1x1(nn.Module):
+    def __init__(self, num_channels):
+        self.conv1 = nn.Conv2d(num_channels, num_channels, 1)
+        self.conv2 = nn.Conv2d(num_channels, num_channels, 1)
+
+    def forward(self, x):
+        inputs = x
+        x = F.relu(x)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        return inputs + x
---
+++ vq_vae_2/examples/hierarchical/train_top.py
@@ -0,0 +1,52 @@
+"""
+Train the top-level prior.
+"""
+
+import itertools
+import os
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from vq_vae_2.examples.hierarchical.data import load_images
+from vq_vae_2.examples.hierarchical.model import TopPrior, make_vae
+from vq_vae_2.examples.hierarchical.train_vae import VAE_PATH, arg_parser
+
+TOP_PRIOR_PATH = 'top.pt'
+
+
+def main():
+    args = arg_parser().parse_args()
+    device = torch.device(args.device)
+
+    vae = make_vae()
+    vae.load_state_dict(torch.load(VAE_PATH, map_location='cpu'))
+    vae.to(device)
+    vae.eval()
+
+    top_prior = TopPrior()
+    if os.path.exists(TOP_PRIOR_PATH):
+        top_prior.load_state_dict(torch.load(TOP_PRIOR_PATH, map_location='cpu'))
+
+    optimizer = optim.Adam(top_prior.parameters())
+    loss_fn = nn.CrossEntropyLoss()
+
+    data = load_images(args.data)
+    for i in itertools.count():
+        images = next(data).to(device)
+        _, _, encoded = vae.encoders[1](vae.encoders[0].encode(images))
+        logits = top_prior(encoded)
+        logits = logits.permute(0, 2, 3, 1).contiguous()
+        logits = logits.view(-1, logits.shape[-1])
+        loss = loss_fn(logits, encoded.view(-1))
+        print('step %d: loss=%f' % (i, loss.item()))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if not i % 10:
+            torch.save(top_prior.state_dict(), TOP_PRIOR_PATH)
+
+
+if __name__ == '__main__':
+    main()

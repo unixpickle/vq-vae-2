68d1a4078dd5804ea5c37c9ea8891705866dc8af
Alex Nichol <unixpickle@gmail.com>
2019-06-18 09:02:51 -0400

    sample in batches

--- vq_vae_2/examples/mnist/sample.py
+++ vq_vae_2/examples/mnist/sample.py
@@ -18,17 +18,19 @@ def main():
     generator = Generator()
     generator.load_state_dict(torch.load('gen.pt'))
 
-    inputs = np.zeros([1, 7, 7], dtype=np.long)
+    inputs = np.zeros([4, 7, 7], dtype=np.long)
     for row in range(7):
         for col in range(7):
             with torch.no_grad():
                 outputs = torch.softmax(generator(torch.from_numpy(inputs)), dim=1).numpy()
-                probs = outputs[0, :, row, col]
-                inputs[0, row, col] = sample_softmax(probs)
+                for i, out in enumerate(outputs):
+                    probs = out[:, row, col]
+                    inputs[i, row, col] = sample_softmax(probs)
         print('done row', row)
     embedded = vae.encoders[0].vq.embed(torch.from_numpy(inputs))
-    decoded = torch.clamp(vae.decoders[0](embedded), 0, 1).detach().numpy()
-    Image.fromarray((decoded * 255).astype(np.uint8)[0, 0]).save('digit.png')
+    decoded = torch.clamp(vae.decoders[0]([embedded]), 0, 1).detach().numpy()
+    decoded = np.concatenate(decoded, axis=1)
+    Image.fromarray((decoded * 255).astype(np.uint8)[0]).save('samples.png')
 
 
 def sample_softmax(probs):

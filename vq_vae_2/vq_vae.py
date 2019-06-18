7ab5113a0ac79a732ee89d4cacb46e9b2c01a7b4
Alex Nichol <unixpickle@gmail.com>
2019-06-18 09:21:24 -0400

    more VQ-VAE encoders/decoders

--- vq_vae_2/vq_vae.py
+++ vq_vae_2/vq_vae.py
@@ -78,6 +78,25 @@ class QuarterEncoder(Encoder):
         return x
 
 
+class HalfEncoder(Encoder):
+    """
+    An encoder that cuts the input size in half in both
+    dimensions.
+    """
+
+    def __init__(self, in_channels, out_channels, num_latents, **kwargs):
+        super().__init__(out_channels, num_latents, **kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels, 3, stride=2, padding=1)
+        self.residual1 = _make_residual(out_channels)
+        self.residual2 = _make_residual(out_channels)
+
+    def encode(self, x):
+        x = self.conv1(x)
+        x = x + self.residual1(x)
+        x = x + self.residual2(x)
+        return x
+
+
 class Decoder(nn.Module):
     """
     An abstract VQ-VAE decoder, which takes a stack of
@@ -125,6 +144,72 @@ class QuarterDecoder(Decoder):
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
+        return x
+
+
+class HalfDecoder(Decoder):
+    """
+    A decoder that upsamples by a factor of 2 in both
+    dimensions.
+    """
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.residual1 = _make_residual(in_channels)
+        self.residual2 = _make_residual(in_channels)
+        self.conv = nn.ConvTranspose2d(in_channels, out_channels, 4, stride=2, padding=1)
+
+    def forward(self, inputs):
+        assert len(inputs) == 1
+        x = inputs[0]
+        x = x + self.residual1(x)
+        x = x + self.residual2(x)
+        x = F.relu(x)
+        x = self.conv(x)
+        return x
+
+
+class HalfQuarterDecoder(Decoder):
+    """
+    A decoder that takes two inputs. The first one is
+    upsampled by a factor of two, and then combined with
+    the second input which is further upsampled by a
+    factor of four.
+    """
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.residual1 = _make_residual(in_channels)
+        self.residual2 = _make_residual(in_channels)
+        self.conv1 = nn.ConvTranspose2d(in_channels, in_channels, 4, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels * 2, in_channels, 3, padding=1)
+        self.residual3 = _make_residual(in_channels)
+        self.residual4 = _make_residual(in_channels)
+        self.conv3 = nn.ConvTranspose2d(in_channels, in_channels, 4, stride=2, padding=1)
+        self.conv4 = nn.ConvTranspose2d(in_channels, out_channels, 4, stride=2, padding=1)
+
+    def forward(self, inputs):
+        assert len(inputs) == 2
+
+        # Upsample the top input to match the shape of the
+        # bottom input.
+        x = inputs[0]
+        x = x + self.residual1(x)
+        x = x + self.residual2(x)
+        x = F.relu(x)
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        # Mix together the bottom and top inputs.
+        x = torch.cat([x, inputs[1]], dim=1)
+        x = self.conv2(x)
+
+        x = x + self.residual3(x)
+        x = x + self.residual4(x)
+        x = F.relu(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv4(x)
         return x
 
 
